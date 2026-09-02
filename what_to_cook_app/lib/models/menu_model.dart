37025f7b66491ffc59a234fc0a7962d9bd886b02
vincent-softdev <vinh.ly.softdev@gmail.com>
2026-09-02 23:49:56 +0930

    add Dummy data and model for menu

--- what_to_cook_app/lib/models/menu_model.dart
+++ what_to_cook_app/lib/models/menu_model.dart
@@ -1,6 +1,17 @@
 class MenuModel {
   final String name;
   final String imageUrl;
+  final String description;
+  final int cookingTime;
+  final double estimatedPrice;
+  final List<String> ingredients;
 
-  MenuModel(this.name, this.imageUrl);
+  MenuModel({
+    required this.name,
+    required this.imageUrl,
+    required this.description,
+    required this.cookingTime,
+    required this.estimatedPrice,
+    required this.ingredients,
+  });
 }
--- what_to_cook_app/lib/screens/menu.dart
+++ what_to_cook_app/lib/screens/menu.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:what_to_cook_app/dummys/menu_data.dart' as menu_data;
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final recipes = menu_data.recipes;
+
     return Scaffold(
       appBar: AppBar(title: const Text('Menu')),
       body: Center(
