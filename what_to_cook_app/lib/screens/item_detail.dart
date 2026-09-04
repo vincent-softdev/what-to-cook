46a41cb49e1e962aa2543b0f181185df87a64c7f
vincent-softdev <vinh.ly.softdev@gmail.com>
2026-09-04 23:41:45 +0930

    init

---
+++ what_to_cook_app/lib/screens/item_detail.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:what_to_cook_app/models/card_model.dart';
+
+class ItemDetail extends StatelessWidget {
+  final CardModel itemDetail;
+
+  const ItemDetail({super.key, required this.itemDetail});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
